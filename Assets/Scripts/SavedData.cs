db7e52ec429d1fdf72b252a707ef952f3f09e3ef
RaySiberian <81709760+RaySiberian@users.noreply.github.com>
2021-11-18 14:31:22 +0300

    Pre

--- Assets/Scripts/SavedData.cs
+++ Assets/Scripts/SavedData.cs
@@ -1,4 +1,7 @@
 using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
 
 [Serializable]
 public class SavedData
@@ -24,5 +27,36 @@ public class SavedData
 
         ReproductionLvl = data.ReproductionLvl;
         MoveSpeedLvl = data.MoveSpeedLvl;
+    }
+}
+
+public class SaveSystem
+{
+    public static void SaveCurrentData(Data data)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/data.save";
+        FileStream stream = new FileStream(path, FileMode.Create);
+        SavedData savedData = new SavedData(data);
+        formatter.Serialize(stream,savedData);
+        stream.Close();
+    }
+
+    public static SavedData LoadData()
+    {
+        string path = Application.persistentDataPath + "/data.save";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+            SavedData savedData = formatter.Deserialize(stream) as SavedData;
+            stream.Close();
+            return savedData;
+        }
+        else
+        {
+            Debug.Log("Not fond file");
+            return null;
+        }
     }
 }
